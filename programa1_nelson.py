43f1dd9e267df98ed6805bd4e9095557d5f0ecb2
25ma <147564791+25ma@users.noreply.github.com>
2025-09-10 11:34:07 -0500

    Add files via upload

---
+++ programa1_nelson.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  6 17:46:40 2025
+
+@author: SALA-1
+"""
+
+numero_uno = 10
+numero_dos = 5
+
+suma = numero_uno + numero_dos
+print("suma =", suma)
+
+
+resta = numero_uno - numero_dos
+print ("resta =",resta)
+
+multiplicacion = numero_uno * numero_dos
+print ("multiplicacion =", multiplicacion)
+
+division = numero_uno / numero_dos 
+print("division =", division)
